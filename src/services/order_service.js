f7a7a65876a3e4f9fc6ff4bb4c92f3e2a77e4c09
Elias Reis <eliasreis54@gmail.com>
2018-11-29 23:55:05 -0200

    some tests added

---
+++ src/services/order_service.js
@@ -0,0 +1,28 @@
+const promotionReduce = (count, factor) => {
+  const min = 3;
+  if (count < min) return count;
+  if (count >= (min * factor)) {
+    if (count >= (min * (factor + 1))) {
+      return promotionReduce(count, factor + 1);
+    }
+    return (count - factor);
+  }
+  return (count - factor);
+};
+
+const light = (bought) => {
+  const bacon = bought.find((element) => {
+    if (element.name === 'Bacon') return true;
+    return false;
+  });
+
+  const lettuce = bought.find((element) => {
+    if (element.name === 'Alface') return true;
+    return false;
+  });
+
+  if ((lettuce) && (!bacon)) return true;
+  return false;
+};
+
+export default { promotionReduce, light };
